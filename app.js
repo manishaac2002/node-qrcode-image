f1fb3cec87b57e46c28cd3a6259cae3219efa9b0
manishaac2002 <manishaac2002@gmail.com>
2023-06-17 08:45:13 +0100

    qrcode-image

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ const qrData = {
 console.log(...qrData);
 // Generate the QR code as a data URL
 // 'H': High (Approximately 30% or less errors can be corrected)
-// qr.toDataURL(JSON.stringify(qrData), { errorCorrectionLevel: 'H' }, (err, qrDataURL) => {
-//   if (err) {
-//     console.error('Error occurred while generating QR code:', err);
-//     return;
-//   }
+qr.toDataURL(JSON.stringify(qrData), { errorCorrectionLevel: 'H' }, (err, qrDataURL) => {
+  if (err) {
+    console.error('Error occurred while generating QR code:', err);
+    return;
+  }
 
-//   // Extract the image data from the data URL
-//   const imageData = qrDataURL.replace(/^data:image\/png;base64,/, '');
+  // Extract the image data from the data URL
+  const imageData = qrDataURL.replace(/^data:image\/png;base64,/, '');
 
-//   // Save the image data as a file
-//   fs.writeFile('qrcode.png', imageData, 'base64', (err) => {
-//     if (err) {
-//       console.error('Error occurred while saving QR code as an image:', err);
-//     } else {
-//       console.log('QR code image saved successfully!');
-//     }
-//   });
-// });
+  // Save the image data as a file
+  fs.writeFile('qrcode.png', imageData, 'base64', (err) => {
+    if (err) {
+      console.error('Error occurred while saving QR code as an image:', err);
+    } else {
+      console.log('QR code image saved successfully!');
+    }
+  });
+});
 
